639985966dc32f6898ccf1dfab7da89ce9d8468e
Mikołaj Klonowski <nick-93@o2.pl>
2018-06-04 19:05:35 +0200

    Lekka zmiana upgrade

--- src/CheckTriangle.java
+++ src/CheckTriangle.java
@@ -1,19 +1,16 @@
 public class CheckTriangle {
-    void isRightTriangle(Triangle triangle) {
+    public boolean isRightTriangle(Triangle triangle) {
         double a = triangle.getBokA() * triangle.getBokA();
         double b = triangle.getBokB() * triangle.getBokB();
         double c = triangle.getBokC() * triangle.getBokC();
 
-        if (triangle.getBokC() > triangle.getBokA() && triangle.getBokC() > triangle.getBokB()) {
-            boolean to = b + a == c;
-            System.out.println(to);
-        } else {
-            boolean nie = 1==0;
-            System.out.println(nie);
-        }
 
-    }
-}
+        return b+a==c;
+
+
+
+
+}}
 
 
 
--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,9 @@
 public class Test {
     public static void main(String[] args) {
-        Triangle triangle1 = new Triangle(12.21,212.32,32.32);
+        Triangle triangle1 = new Triangle(12,5,13);
         CheckTriangle check = new CheckTriangle();
-        check.isRightTriangle(triangle1);
+        System.out.println(check.isRightTriangle(triangle1));
+
 
     }
 }
